9e0686ecedb82cc4afd598233d902568f8d3d397
Diogo L Scarmagnani <diogols@live.com>
2023-08-17 13:33:18 -0300

    visibilitychange

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-const main = document.querySelector('main')
-const button = document.querySelector('button')
-const dialog = document.querySelector('dialog')
-const audio = document.querySelector('audio')
+const main    = document.querySelector('main')
+const button  = document.querySelector('button')
+const dialog  = document.querySelector('dialog')
+const audio   = document.querySelector('audio')
 
 button.addEventListener('click', () => {
   main.classList.add('apaga-tela')
   audio.play()
   setTimeout(() => dialog.showModal(), 2000)
+})
+
+document.addEventListener('visibilitychange', () => {
+  if (!dialog.open)
+    return
+  
+  if (document.visibilityState === 'hidden') {
+    audio.pause()
+    return
+  }
+
+  audio.play()
 })
